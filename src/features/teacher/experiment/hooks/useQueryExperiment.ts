9305afe6639a970a3c67d823ad4257e5e575bb96
zph <zphhpzzph@gmail.com>
2026-01-28 18:58:00 +0800

    refactor: 重构实验查询钩子，简化代码结构

--- src/features/teacher/experiment/hooks/useQueryExperiment.ts
+++ src/features/teacher/experiment/hooks/useQueryExperiment.ts
@@ -1,53 +1,19 @@
 import { getApiTeacherExperiments } from "@/core/api/generated";
-import type { GetApiParamsTypeBase } from "@/core/utils/typeUtils";
 import type { QueryOptions } from "@/features/shared/types/UseQueryOptions";
 import { useQuery } from "@tanstack/vue-query";
-import { computed, ref } from "vue";
 import client from "@/core/api/config";
 
-export default function useQueryExperimentBase(
-  queryParams: GetApiParamsTypeBase<typeof getApiTeacherExperiments, "params">,
-  options: QueryOptions,
-) {
+/**
+ * 查询所有实验
+ */
+export function useQueryExperimentAll(options?: Partial<QueryOptions>) {
   return useQuery({
-    queryKey: options.queryKey,
+    queryKey: options?.queryKey || ["experiments-all"],
     queryFn: () =>
       getApiTeacherExperiments({
-        params: queryParams,
         client,
       }),
     select: (res) => res.data?.data,
+    enabled: options?.enable,
   });
-}
-
-export function useQueryExperimentAll() {
-  const query = useQueryExperimentBase(
-    {},
-    {
-      queryKey: ["experiments-all"],
-    },
-  );
-  return { query };
-}
-
-export function useQueryExperimentPage(initial: {
-  current?: number;
-  size?: number;
-}) {
-  const current = ref(initial.current || 1);
-  const size = ref(initial.size || 20);
-  const query = useQueryExperimentBase(
-    {
-      current: current.value,
-      size: size.value,
-    },
-    {
-      queryKey: computed(() => ["experiments-page", current.value, size.value]),
-    },
-  );
-  return {
-    current,
-    size,
-    query,
-  };
 }
